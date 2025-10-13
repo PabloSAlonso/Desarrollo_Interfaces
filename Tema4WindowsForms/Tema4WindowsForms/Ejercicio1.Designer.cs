731514ef2a139aae04a5832b2ec49770293a1ef0
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-13 13:16:51 +0200

    changes

--- Tema4WindowsForms/Tema4WindowsForms/Ejercicio1.Designer.cs
+++ Tema4WindowsForms/Tema4WindowsForms/Ejercicio1.Designer.cs
@@ -40,8 +40,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ejercicio1_MouseMove);
             // 
             // button2
             // 
@@ -51,7 +50,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Ejercicio1_MouseMove);
             // 
             // Ejercicio1
             // 
--- Tema4WindowsForms/Tema4WindowsForms/Ejercicio1.cs
+++ Tema4WindowsForms/Tema4WindowsForms/Ejercicio1.cs
@@ -16,45 +16,20 @@ namespace Tema4WindowsForms
         {
             InitializeComponent();
         }
-        
 
-        
+
+
+        private void Ejercicio1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = $"X: {MousePosition.X}, Y: {MousePosition.Y}";
+        }
 
         private void Ejercicio1_MouseLeave(object sender, EventArgs e)
         {
             this.Text = "Mouse Tester";
         }
 
-        private void Ejercicio1_MouseMove(object sender, MouseEventArgs e)
-        {
-            int x = e.X;
-            int y = e.Y;
-            this.Text = $"X: {x}, Y: {y}";
-        }
-
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_MouseMove(object sender, MouseEventArgs e)
-        {
-            int x = e.X;
-            int y = e.Y;
-            int xbtn = this.button1.Location.X;
-            int ybtn = this.button1.Location.Y;
-            this.Text = $"X: {xbtn + x} Y:{ybtn + y}";
-        }
-
-        private void button2_MouseMove(object sender, MouseEventArgs e)
-        {
-            int x = e.X;
-            int y = e.Y;
-            int xbtn = this.button2.Location.X;
-            int ybtn = this.button2.Location.Y;
-            this.Text = $"X: {xbtn + x} Y:{ybtn + y}";
-        }
+       
     }
 }
+

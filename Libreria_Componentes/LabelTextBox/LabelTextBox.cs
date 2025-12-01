5415e148b1be12c14dca41ee037cd945c775d7c4
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-12-01 13:59:37 +0100

    changes

---
+++ Libreria_Componentes/LabelTextBox/LabelTextBox.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelTextBox
+{
+    public enum EPosicion
+    {
+        IZQUIERDA, DERECHA
+    }
+    [DefaultProperty("TextLbl"),DefaultEvent("Load")]
+    public partial class LabelTextBox : UserControl
+    {
+
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private EPosicion posicion = EPosicion.IZQUIERDA;
+        [Category("Appearance")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del Textbox")]
+        public EPosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(EPosicion), value))
+                {
+                    posicion = value;
+                    OnPosicionChanged(EventArgs.Empty);
+                    Recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+
+        //Pixeles de separación entre label y textbox
+        private int separacion = 0;
+        [Category("Design")] // O se puede meter en categoría Design.
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    Recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Mis Propiedades")] // O se puede meter en categoría Appearance.
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set { lbl.Text = value; }
+            get { return lbl.Text; }
+        }
+
+        [Category("Mis Propiedades")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextTxt
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+
+        private void Recolocar()
+        {
+            switch (posicion)
+            {
+                case EPosicion.IZQUIERDA:
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(0, 0);
+                    // Establecemos posición componente txt
+                    txt.Location = new Point(lbl.Width + Separacion, 0);
+                    //Establecemos ancho del Textbox
+                    //(la label tiene ancho por autosize)
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos altura del componente
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                case EPosicion.DERECHA:
+                    //Establecemos posición del componente txt
+                    txt.Location = new Point(0, 0);
+                    //Establecemos ancho del Textbox
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(txt.Width + Separacion, 0);
+                    //Establecemos altura del componente (Puede sacarse del switch)
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Recolocar();
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnKeyPress(e); //Enlazar Formulario con texto del TextBox del LabelTextBox
+        }
+
+        [Category("La propiedad cambió")]
+        [Description("Se lanza cuando la propiedad Posicion cambia")]
+        public event System.EventHandler PosicionChanged;
+        protected virtual void OnPosicionChanged(EventArgs e)
+        {
+            if (PosicionChanged != null)
+            {
+                PosicionChanged(this, e);
+            }
+        }
+    }
+}
---
+++ Libreria_Componentes/PruebasComponentes/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PruebasComponentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelTextBox1 = new LabelTextBox.LabelTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelTextBox1
+            // 
+            this.labelTextBox1.Location = new System.Drawing.Point(237, 85);
+            this.labelTextBox1.Name = "labelTextBox1";
+            this.labelTextBox1.Posicion = LabelTextBox.EPosicion.IZQUIERDA;
+            this.labelTextBox1.Separacion = 60;
+            this.labelTextBox1.Size = new System.Drawing.Size(295, 20);
+            this.labelTextBox1.TabIndex = 2;
+            this.labelTextBox1.TextLbl = "label1";
+            this.labelTextBox1.TextTxt = "";
+            this.labelTextBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.labelTextBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pruebas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private LabelTextBox.LabelTextBox labelTextBox1;
+    }
+}
+

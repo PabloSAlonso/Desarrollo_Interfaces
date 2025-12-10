75541e80df28d2d00ab8ae0bc7e9777a8aa7e88b
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-12-10 17:52:08 +0100

    changes

--- Libreria_Componentes/LabelTextBox/EtiquetaAviso.cs
+++ Libreria_Componentes/LabelTextBox/EtiquetaAviso.cs
@@ -18,24 +18,72 @@ namespace LabelTextBox
     }
     public partial class EtiquetaAviso : Control
     {
+        private EMarca marca = EMarca.Nada;
+        [Category("Appearance")]
+        [Description("Indica el tipo de marca que aparece junto al texto")]
+        public EMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get
+            {
+                return marca;
+            }
+        }
         public EtiquetaAviso()
         {
             InitializeComponent();
         }
 
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-            Graphics g = pe.Graphics; //Lienzo del componente
-            SolidBrush b = new SolidBrush(this.ForeColor); //Pincel de un solo color sin texturas ni gradientes
-            g.DrawString(this.Text, this.Font, b, 0, 0); //Pinta String
-            this.Size = g.MeasureString(this.Text, this.Font).ToSize(); //Measure devuelve size en float (sirve para comprobar la longitud de un string), se pasa a size normal con ToSize()
-
-        }
         protected override void OnTextChanged(EventArgs e)
         {
             base.OnTextChanged(e);
             this.Refresh();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0; //Grosor de las líneas de dibujo
+            int offsetX = 0; //Desplazamiento a la derecha del texto
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+                             // Altura de fuente, usada como referencia en varias partes
+            int h = this.Font.Height;
+            //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
+            // a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+            switch (Marca)
+            {
+                case EMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor,
+                    h, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor;
+                    break;
+                case EMarca.Cruz:
+                    grosor = 3;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, h, h);
+                    g.DrawLine(lapiz, h, grosor, grosor, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se
+                    //pueden realizar muchos y cogen memoria
+                    lapiz.Dispose();
+                    break;
+            }
+            //Finalmente pintamos el Texto; desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+            b.Dispose();
         }
     }
 }
---
+++ Libreria_Componentes/PruebasComponentes/Prueba_EtiquetaAviso.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PruebasComponentes
+{
+    partial class Prueba_EtiquetaAviso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.etiquetaAviso1 = new LabelTextBox.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Location = new System.Drawing.Point(91, 50);
+            this.etiquetaAviso1.Marca = LabelTextBox.EMarca.Cruz;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(97, 15);
+            this.etiquetaAviso1.TabIndex = 0;
+            this.etiquetaAviso1.Text = "etiquetaAviso1";
+            // 
+            // Prueba_EtiquetaAviso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Name = "Prueba_EtiquetaAviso";
+            this.Text = "Prueba_EtiquetaAviso";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LabelTextBox.EtiquetaAviso etiquetaAviso1;
+    }
+}

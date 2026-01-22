4472f0ae41533844c7bf2db527ebf0764b865861
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-22 11:54:45 +0100

    feat started exercise 3

---
+++ Libreria_Componentes/LabelTextBox/MultiMediaReproductor.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LabelTextBox
+{
+    partial class MultiMediaReproductor
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReproducir = new System.Windows.Forms.Button();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReproducir
+            // 
+            this.btnReproducir.Location = new System.Drawing.Point(3, 3);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(212, 183);
+            this.btnReproducir.TabIndex = 0;
+            this.btnReproducir.Text = "Play";
+            this.btnReproducir.UseVisualStyleBackColor = true;
+            this.btnReproducir.Click += new System.EventHandler(this.btnReproducir_Click);
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(221, 3);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(42, 13);
+            this.lblTiempo.TabIndex = 1;
+            this.lblTiempo.Text = "MM:SS";
+            // 
+            // MultiMediaReproductor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.btnReproducir);
+            this.Name = "MultiMediaReproductor";
+            this.Size = new System.Drawing.Size(268, 190);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReproducir;
+        private System.Windows.Forms.Label lblTiempo;
+    }
+}
---
+++ Libreria_Componentes/LabelTextBox/MultiMediaReproductor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelTextBox
+{
+    public partial class MultiMediaReproductor : UserControl
+    {
+        string formato;
+        public MultiMediaReproductor()
+        {
+            InitializeComponent();
+            formato = $"{Minutos:00}:{Segundos:00}";
+        }
+
+        [Category("Acción")]
+        [Description("Evento que se lanza al pulsar el boton Play / Stop del componente")]
+        public event EventHandler PlayClick;
+        protected virtual void OnPlayClick(object sender, EventArgs e)
+        {
+            if (PlayClick != null)
+            {
+                PlayClick(this, EventArgs.Empty);
+            }
+        }
+
+        private void btnReproducir_Click(object sender, EventArgs e)
+        {
+            if (sender == btnReproducir)
+            {
+                btnReproducir.Text = btnReproducir.Text == "Play" ? "Stop" : "Play";
+                OnPlayClick(sender, e);
+            }
+        }
+
+        [Category("Propiedades etiqueta")]
+        [Description("Representa los minutos de la etiqueta")]
+        public int Minutos = 0;
+        private int minutos
+        {
+            set
+            {
+                if (minutos > 59)
+                {
+                    minutos = 0;
+                }
+                else
+                {
+                    minutos = value;
+                }
+            }
+            get { return minutos; }
+        }
+
+        [Category("Propiedades etiqueta")]
+        [Description("Representa los segundos de la etiqueta")]
+        public int Segundos = 0;
+        private int segundos
+        {
+            set
+            {
+                if (segundos > 59)
+                {
+                    if (segundos != 0 && segundos % 60 == 0)
+                    {
+                        OnDesbordaTiempo(this, EventArgs.Empty);
+                    }
+                    segundos = value % 60;
+                    lblTiempo.Text = formato;
+                }
+                else
+                {
+                    segundos = value;
+                    lblTiempo.Text = formato;
+                }
+            }
+            get { return segundos; }
+        }
+
+        public event EventHandler DesbordaTiempo;
+
+        protected virtual void OnDesbordaTiempo(object sender, EventArgs e) // Esto se lanza en la variable segundos, no lo lanza otro método
+        {
+            if (DesbordaTiempo != null)
+            {
+                DesbordaTiempo(this, EventArgs.Empty);
+            }
+
+        }
+    }
+}
